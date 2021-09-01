d42f7b7629b68316a966742ef0aff88443d59ed7
SENA <regionalatlanticoappmovil@gmail.com>
2021-09-01 14:19:04 -0500

    SecondCommit

---
+++ app/src/main/java/com/example/apppruebaatlantico/Login.kt
@@ -0,0 +1,52 @@
+package com.example.apppruebaatlantico
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.apppruebaatlantico.DataBase.AdminSQLiteOpenHelper
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val preferencias=getSharedPreferences("usuarios", Context.MODE_PRIVATE)
+        val email=findViewById<EditText>(R.id.etemail)
+        val pass=findViewById<EditText>(R.id.etcontraseña)
+        email.setText(preferencias.getString("user",null))
+        pass.setText(preferencias.getString("pass",null))
+        findViewById<Button>(R.id.ingresar).setOnClickListener {
+            if (!email.text.isEmpty() && !pass.text.isEmpty()){
+                val admin= AdminSQLiteOpenHelper(this,"restaurante",null,1)
+                val bd=admin.writableDatabase
+                val consulta=bd.rawQuery("SELECT contraseña FROM usuarios WHERE email='${email.text}'",null)
+                if(consulta.moveToFirst()){
+                    if (consulta.getString(0).equals(pass.text.toString())){
+                        val editor=preferencias.edit()
+                        editor.putString("user",email.text.toString())
+                        editor.putString("pass",pass.text.toString())
+                        editor.commit()
+                        Toast.makeText(this,"se le a activado el recuerdame en su cuenta",Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,especialidadCasa::class.java))
+                    }else{
+                        Toast.makeText(this,"Contraseña incorrecta",Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(this,"Este usuario no fue encontrado",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Faltan campos por llenar!",Toast.LENGTH_SHORT).show()
+            }
+        }
+        findViewById<Button>(R.id.registrar).setOnClickListener {
+            val intent=Intent(this,Registrar::class.java)
+            startActivity(intent)
+            findViewById<EditText>(R.id.etemail).setText(null)
+            findViewById<EditText>(R.id.etcontraseña).setText(null)
+        }
+    }
+
+}
--- app/src/main/java/com/example/apppruebaatlantico/MainActivity.kt
+++ app/src/main/java/com/example/apppruebaatlantico/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.apppruebaatlantico
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread.sleep(1000)
+        startActivity(Intent(this,Login::class.java))
+        val preferencias=getSharedPreferences("politicas", Context.MODE_PRIVATE)
+        val editor=preferencias.edit()
+        editor.putString("texto","Entiendo que, en consecuencia, el Restaurante es responsable por asegurar la concordancia entre los datos que le han sido suministrado y los que registra/divulga, pero no tiene ninguna responsabilidad por la calidad y veracidad de los datos reportados.\\nEs claro para mí que, por medio de esta consulta de datos, el Restaurante pone a mi alcance los mecanismos necesarios para que pueda ejercer el derecho de habeas data, de acuerdo con lo establecido en el artículo 15 de la Constitución Política de Colombia, la Ley 1581 de 2012 y de acuerdo con la doctrina de la Corte Constitucional de Colombia.\\nPuede consultar nuestra Política de Privacidad y Protección de Datos Personales")
+        editor.commit()
     }
 }
